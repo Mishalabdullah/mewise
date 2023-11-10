bf5ff885f2d1164383cf5fae39f07a294cac410e
Mishalabdullah <github.ijvob@simplelogin.co>
2023-11-10 11:51:48 +0530

    sending emails via cron

--- data/cron.py
+++ data/cron.py
@@ -2,7 +2,9 @@ from django_cron import CronJobBase, Schedule
 from django.core.mail import send_mail
 from .models import Quotes
 from django.contrib.auth.models import User
-
+from django.conf import settings
+from django.core.mail import send_mail
+import decouple
 class SendDataEmails(CronJobBase):
     RUN_EVERY_MINS = 2
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
@@ -13,12 +15,12 @@ class SendDataEmails(CronJobBase):
         email_content = '\n'.join([f'{data.title}: {data.description}' for data in selected_data])
 
         users = User.objects.all()
-        sender_email = 'mishalabdullah@protonmail.com'
+        sender_email =decouple.config("EMAIL_HOST_USER")
 
         for user in users:
             print(f"Sending email to: {user.email}")  # Check recipient email
             result = send_mail(
-                'Selected Data',
+                'Your Bits Of the Day',
                 email_content,
                 sender_email, 
                 [user.email],
